13248006f072abe1fd35df9828da44c2a21b9166
Calvin Kipkirui <calvinkipkirui99@gmail.com>
2024-11-28 16:39:33 +0300

    frontend created

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -9,7 +9,7 @@ function Sidebar() {
   const [isFarmersOpen, setIsFarmersOpen] = useState(false); // Dropdown state for "Farmers"
 
   return (
-    <div className="w-40 h-full bg-gray-200 text-black p-5 fixed left-0 top-0 flex flex-col">
+    <div className="w-41 h-full bg-gray-200 text-black p-5 fixed left-0 top-0 flex flex-col">
       <h2 className="text-2xl font-bold text-center mb-8">FarmFlow</h2>
       <ul className="flex-grow">
         <li className="mb-4 flex items-center">
